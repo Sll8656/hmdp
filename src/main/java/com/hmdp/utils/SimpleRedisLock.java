fda89a751dafa569c024d3ae03e9e38a675e1ecf
Sll <17856518073@163.com>
2023-12-21 11:24:10 +0800

    解决简易分布式锁的误删问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,11 @@
 package com.hmdp.utils;
 
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
+
 
 public class SimpleRedisLock implements ILock{
     private String name;
@@ -15,15 +17,20 @@ public class SimpleRedisLock implements ILock{
     }
 
     private static final String KEY_PREFIX = "lock:";
+    private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     public boolean tryLock(long timeoutSec) {
         Long threadId = Thread.currentThread().getId();
-
+        String newThreadId = ID_PREFIX + threadId;
         Boolean success = stringRedisTemplate.opsForValue()
-                .setIfAbsent(KEY_PREFIX+name,threadId+"", timeoutSec, TimeUnit.SECONDS);
+                .setIfAbsent(KEY_PREFIX+name,newThreadId, timeoutSec, TimeUnit.SECONDS);
         return Boolean.TRUE.equals(success);
     }
 
     public void unlock() {
-        stringRedisTemplate.delete(KEY_PREFIX + name);
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        String newThreadId = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+        if(threadId.equals(newThreadId)) {
+            stringRedisTemplate.delete(KEY_PREFIX + name);
+        }
     }
 }
